c8c5d94b7ce5e07732914be277b6aacca59a40eb
Harsh Kumar <yadavharsh1201@gmail.com>
2025-04-15 10:29:51 +0530

    add weight category based on BMI

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', function(e){
+// Prevent the default behaviour(page refresh on submit, etc..) of the form.
     e.preventDefault();
 
+// store the value of height & weight only when the 'submit' event occurs. 
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector("#weight").value);
     const results = document.querySelector('#results');
@@ -12,6 +14,14 @@ form.addEventListener('submit', function(e){
         results.innerHTML = "Please give a valid weight!"
     } else {
         const bmi = (weight / ((height*height) / 10000)).toFixed(2);
-        results.innerHTML = `<span>BMI: ${bmi}</span>` 
+
+// Print the message for weight category based on BMI.
+        if( bmi < 18.5 ){
+            results.innerHTML = `<span>BMI: ${bmi} <br> Underweight</span>`;
+        } else if( bmi >= 18.5 && bmi < 25 ){
+            results.innerHTML = `<span>BMI: ${bmi} <br> Normalweight</span>`;
+        } else {
+            results.innerHTML = `<span>BMI: ${bmi} <br> Overweight</span>`;
+        } 
     } 
 })
